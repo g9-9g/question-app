424e35264612bc535cf749b20582e25b10bdc130
buianhduc <buianhduc.popodoo2@gmail.com>
2020-11-14 21:50:49 +0700

    create question page

---
+++ templates/questionPage.js
@@ -0,0 +1,90 @@
+let data = [
+    {
+        "questionTitle": "What is this instrument?",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSAB07LPkteouZ-DsSY4HNiDBgaHy66XdYkbQ&usqp=CAU",
+        "answers": [
+            {
+                "value" : "bruh",
+                "isTrue" : true
+            },
+            {
+                "value" : "bruh",
+                "isTrue" : false
+            },
+            {
+                "value" : "bruh",
+                "isTrue" : false
+            },
+            {
+                "value" : "bruh",
+                "isTrue" : false
+            }
+        ]
+    },
+    {
+        "questionTitle": "What is this instrument2?",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSAB07LPkteouZ-DsSY4HNiDBgaHy66XdYkbQ&usqp=CAU",
+        "answers": [
+            {
+                "value" : "bruh",
+                "isTrue" : true
+            },
+            {
+                "value" : "bruh",
+                "isTrue" : false
+            },
+            {
+                "value" : "bruh",
+                "isTrue" : false
+            },
+            {
+                "value" : "bruh",
+                "isTrue" : false
+            }
+        ]
+    },
+    {
+        "questionTitle": "What is this instrument?3",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSAB07LPkteouZ-DsSY4HNiDBgaHy66XdYkbQ&usqp=CAU",
+        "answers": [
+            {
+                "value" : "bruh",
+                "isTrue" : true
+            },
+            {
+                "value" : "bruh",
+                "isTrue" : false
+            },
+            {
+                "value" : "bruh",
+                "isTrue" : false
+            },
+            {
+                "value" : "bruh",
+                "isTrue" : false
+            }
+        ]
+    },
+    {
+        "questionTitle": "What is this instrument?4",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSAB07LPkteouZ-DsSY4HNiDBgaHy66XdYkbQ&usqp=CAU",
+        "answers": [
+            {
+                "value" : "bruh",
+                "isTrue" : true
+            },
+            {
+                "value" : "bruh",
+                "isTrue" : false
+            },
+            {
+                "value" : "bruh",
+                "isTrue" : false
+            },
+            {
+                "value" : "bruh",
+                "isTrue" : false
+            }
+        ]
+    }
+]
